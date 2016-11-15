801a41f94f108292f67bdccd69a2c955cb2a0d50
Chris Chambers <magma.chambers@gmail.com>
2016-11-15 19:42:25 +0000

    Bootstrapping this project on a 7 year old macbook pro is pain.

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,6 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * First attempt at a react-native view
+ * Chris Chambers <magma.chambers@gmail.com>
  * @flow
  */
 
@@ -8,6 +8,7 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Image,
   Text,
   View
 } from 'react-native';
@@ -15,38 +16,16 @@ import {
 export default class PlayablScoreScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Image source={require('./app/img/backgroundeffect.png')} 
+             style={styles.backgroundImage} 
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
