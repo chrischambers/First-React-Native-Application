320676871079afc96baff73d2c8fdcbbddace336
Chris Chambers <magma.chambers@gmail.com>
2016-11-17 00:27:18 +0000

    Refactor ResultCard to use stateless functional components

--- app/components/ResultCard.js
+++ app/components/ResultCard.js
@@ -7,58 +7,46 @@ import {
 
 import { AppText } from './CommonElements'
 
-
-class Outcome extends React.Component {
-  render() {
-    let isDraw = !this.props.winner
-    if (isDraw) {
-      return (
-        <View style={styles.outcome}>
-          <AppText style={styles.outcomeText}>Draw</AppText>
-        </View>
-      )
-    } else {
-      const color = this.props.winner.color
-      return (
-        <View style={[styles.outcome, {backgroundColor: color}]}>
-          <Image source={{uri: this.props.winner.logo}}
-                 style={styles.logoLarge} />
-          <AppText style={styles.outcomeText}>
-            Win
-          </AppText>
-        </View>
-      )
-    }
-  }
-}
-
-
-class CollectAmount extends React.Component {
-  render() {
+const Outcome = props => {
+  let isDraw = !props.winner
+  if (isDraw) {
     return (
-      <View style={styles.collectAmount}>
-        <AppText style={styles.collectAmountText}>COLLECT {this.props.collect}</AppText>
-        <Image source={require('../img/coin.png')} 
-             style={styles.coin} />
+      <View style={styles.outcome}>
+        <AppText style={styles.outcomeText}>Draw</AppText>
+      </View>
+    )
+  } else {
+    const color = props.winner.color
+    return (
+      <View style={[styles.outcome, {backgroundColor: color}]}>
+        <Image source={{uri: props.winner.logo}} style={styles.logoLarge} />
+        <AppText style={styles.outcomeText}>Win</AppText>
       </View>
     )
   }
 }
 
-CollectAmount.propTypes = {
-  collect: React.PropTypes.number.isRequired,
+const CollectAmount = props => {
+  return (
+    <View style={styles.collectAmount}>
+      <AppText style={styles.collectAmountText}>COLLECT {props.collect}</AppText>
+      <Image source={require('../img/coin.png')} style={styles.coin} />
+    </View>
+  )
 }
 
-export default class ResultCard extends React.Component {
-  render() {
-    return (
-      <View style={styles.resultCard}>
-        <Outcome winner={this.props.winner} loser={this.props.loser}/>
-        <CollectAmount collect={this.props.collect} />
-      </View>
-    )
-  }
+const ResultCard = props => {
+  return (
+    <View style={styles.resultCard}>
+      <Outcome winner={props.winner} loser={props.loser}/>
+      <CollectAmount collect={props.collect} />
+    </View>
+  )
 }
+
+propTypes = {collect: React.PropTypes.number.isRequired}
+ResultCard.propTypes = CollectAmount.PropTypes = propTypes
+export default ResultCard
 
 const styles = StyleSheet.create({
   resultCard: {
